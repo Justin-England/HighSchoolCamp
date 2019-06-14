32926ce14b70f69f770da2d6a3774a99282e04e4
Justin England <justin.b.england@gmail.com>
2019-06-14 14:23:23 -0500

    Added basketballboi

--- pygame-practice.py
+++ pygame-practice.py
@@ -33,52 +33,86 @@ pygame.display.set_caption("Shape Set Up")
 done = False
 clock = pygame.time.Clock()
 
+
+
+
+# Speed in pixels per frame
+x_speed = 0
+y_speed = 0
+
+# Current position
+x_coord = 10
+y_coord = 10
+
+ball_pos = 0
+ball_change = 10
+def draw_stick_figure(screen, x, y):
+    #Here is my work!
+#Lenny face!
+    #face and eyes
+    pygame.draw.ellipse(screen, WHITE, [300 + x, 500 + y, 400, 200])
+    pygame.draw.arc(screen, BLACK, [340 + x, 510 + y, 100, 170], ((2 / 3) * PI), ((4 / 3) * PI), 15)
+    pygame.draw.arc(screen, BLACK, [560 + x, 510 + y, 100, 170], ((5 / 3) * PI), ((1 / 3) * PI), 15)
+    pygame.draw.ellipse(screen, BLACK, [400 + x, 520 + y, 50, 50], 10)
+    pygame.draw.ellipse(screen, BLACK, [545 + x, 520 + y, 50, 50], 10)
+    #eyebrows
+    pygame.draw.arc(screen, BLACK, [365 + x, 510 + y, 150, 100], ((1/4) * PI), ((3/4) * PI), 10)
+    pygame.draw.arc(screen, BLACK, [505 + x, 510 + y, 150, 100], ((1 / 4) * PI), ((3 / 4) * PI), 10)
+    #nose
+    pygame.draw.line(screen, BLACK, [505 + x, 540 + y], [505 + x, 580 + y], 15)
+    pygame.draw.arc(screen, BLACK, [480 + x, 575 + y, 50, 60], PI, ((1 / 2) * PI), 10)
+    #mouth
+    pygame.draw.arc(screen, BLACK, [450 + x, 610 + y, 110, 65], PI, (2 * PI), 10)
+#Body!
+    #torso
+    pygame.draw.line(screen, WHITE, [505 + x, 700 + y], [505 + x, 1010 + y], 20)
+    #Legs
+        #Right
+    pygame.draw.arc(screen, WHITE, [400 + x, 1000 + y, 200, 500], ((2) * PI), ((1 / 2) * PI), 20)
+        #Left
+    pygame.draw.arc(screen, WHITE, [400 + x, 1000 + y, 200, 500], ((1 / 2) * PI), PI, 20)
+    #Arms
+        #Right
+    pygame.draw.arc(screen, WHITE, [300 + x, 700 + y, 400, 300], ((7 / 4) * PI), ((1 / 2) * PI), 20)
+        #Left
+    pygame.draw.arc(screen, WHITE, [300 + x, 700 + y, 400, 300], ((1 / 2) * PI), ((5/4) * PI), 20)
+    #Ball!
+    pygame.draw.ellipse(screen, RED, [320 + x, 920 + ball_pos + y, 50, 50])
+    
 # Loop as long as done == False
 while not done:
-
+    ball_pos += ball_change
+    if ball_pos > 280:
+        ball_change -= 10
+    elif ball_pos < 1:
+        ball_change -= -10
+    # User did something
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x_speed = -30
+    if keys[pygame.K_RIGHT]:
+        x_speed = 30
+    if keys[pygame.K_UP]:
+        y_speed = -30
+    if keys[pygame.K_DOWN]:
+        y_speed = 30
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
+    # Move the object according to the speed vector.
+    x_coord += x_speed
+    y_coord += y_speed
 
+    # Reset x_speed and y_speed for the next frame
+    x_speed = 0
+    y_speed = 0
     # All drawing code happens after the for loop and but
     # inside the main while not done loop.
 
     # Clear the screen and set the screen background
     screen.fill(BLACK)
 
-    #Here is my work!
-#Lenny face!
-    #face and eyes
-    pygame.draw.ellipse(screen, WHITE, [300, 500, 400, 200])
-    pygame.draw.arc(screen, BLACK, [340, 510, 100, 170], ((2 / 3) * PI), ((4 / 3) * PI), 15)
-    pygame.draw.arc(screen, BLACK, [560, 510, 100, 170], ((5 / 3) * PI), ((1 / 3) * PI), 15)
-    pygame.draw.ellipse(screen, BLACK, [400, 520, 50, 50], 10)
-    pygame.draw.ellipse(screen, BLACK, [545, 520, 50, 50], 10)
-    #eyebrows
-    pygame.draw.arc(screen, BLACK, [365, 510, 150, 100], ((1/4) * PI), ((3/4) * PI), 10)
-    pygame.draw.arc(screen, BLACK, [505, 510, 150, 100], ((1 / 4) * PI), ((3 / 4) * PI), 10)
-    #nose
-    pygame.draw.line(screen, BLACK, [505, 540], [505, 580], 15)
-    pygame.draw.arc(screen, BLACK, [480, 575, 50, 60], PI, ((1 / 2) * PI), 10)
-    #mouth
-    pygame.draw.arc(screen, BLACK, [450, 610, 110, 65], PI, (2 * PI), 10)
-#Body!
-    #torso
-    pygame.draw.line(screen, WHITE, [505, 700], [505, 1010], 20)
-    #Legs
-        #Right
-    pygame.draw.arc(screen, WHITE, [400, 1000, 200, 500], ((2) * PI), ((1 / 2) * PI), 20)
-        #Left
-    pygame.draw.arc(screen, WHITE, [400, 1000, 200, 500], ((1 / 2) * PI), PI, 20)
-    #Arms
-        #Right
-    pygame.draw.arc(screen, WHITE, [400, 700, 200, 500], ((2) * PI), ((1 / 2) * PI), 20)
-        #Left
-    pygame.draw.arc(screen, WHITE, [400, 700, 200, 500], ((1 / 2) * PI), PI, 20)
-
-
-
-    #Work stops here.
+    draw_stick_figure(screen, x_coord, y_coord)
 
 
 
@@ -90,7 +124,7 @@ while not done:
     # Render the text. "True" means anti-aliased text.
     # Black is the color. This creates an image of the
     # letters, but does not put it on the screen
-    text = font.render("My text", True, BLACK)
+    text = font.render("I'm a baller.", True, WHITE)
 
     # Put the image of the text on the screen at 250x250
     screen.blit(text, [250, 250])
